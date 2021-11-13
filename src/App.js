8d64c98802bf1bcfbc983774cb164e58ffa4d706
robinsahu <sahurobin1497@gmail.com>
2021-11-13 23:44:12 +0530

    updating our app with a shop page , collection preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.componenet";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./menu-item.styles.scss";
+// import { withRouter } from "react-router"; //it is kind of a function or high order component which takes another component as an argument and gives powered up componet and it can have than all props loke match,history related to that route
 
 const MenuIteam = ({ title, imageUrl, size }) => {
   //you can use destructuring as well in argument directoly rather than use "props"
